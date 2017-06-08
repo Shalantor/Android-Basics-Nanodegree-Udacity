aaaf2b84dea015052056cc868bee0399327381e2
shalantor <georgkaraolanis@gmail.com>
2017-06-08 22:27:52 +0300

    Project6: Change variables and types in custom class

--- Project6-TourGuideApp/app/src/main/java/com/example/georgekaraolanis/project6_tourguideapp/Location.java
+++ Project6-TourGuideApp/app/src/main/java/com/example/georgekaraolanis/project6_tourguideapp/Location.java
@@ -4,63 +4,68 @@ package com.example.georgekaraolanis.project6_tourguideapp;
 public class Location {
 
     /*Variables of location*/
-    private String phoneNumber = null;
-    private String address = null;
-    private String rating = null;
-    private String description = null;
-    private int imageResourceId = NO_IMAGE_PROVIDED;
+    private int nameId = NO_SOURCE_PROVIDED;
+    private int phoneNumberId = NO_SOURCE_PROVIDED;
+    private int addressId = NO_SOURCE_PROVIDED;
+    private int descriptionId = NO_SOURCE_PROVIDED;
+    private int ratingId = NO_SOURCE_PROVIDED;
+    private int imageResourceId = NO_SOURCE_PROVIDED;
 
     /* Constant value that represents no image was provided for this word */
-    private static final int NO_IMAGE_PROVIDED = -1;
+    private static final int NO_SOURCE_PROVIDED = -1;
 
     /*Constructor, initiate variables for Restaurants*/
-    public Location(String phoneNumber,String address,String rating,
-                    String description){
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.rating = rating;
-        this.description = description;;
+    public Location(int nameId,int phoneNumberId,int addressId,
+                    int descriptionId,int ratingId){
+        this.nameId = nameId;
+        this.phoneNumberId = phoneNumberId;
+        this.addressId = addressId;
+        this.descriptionId = descriptionId;
+        this.ratingId = ratingId;
     }
 
-    /*Constructor, initiate variables For Hotels*/
-    public Location(String phoneNumber,String address,String rating){
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-        this.rating = rating;
+    /*Constructor, initiate variables for Hotels*/
+    public Location(int nameId,int phoneNumberId,int addressId, int ratingId){
+        this.nameId = nameId;
+        this.phoneNumberId = phoneNumberId;
+        this.addressId = addressId;
+        this.ratingId = ratingId;
     }
 
     /*Constructor, initiate variables for Beaches*/
-    public Location(String address,int imageResourceId){
-        this.address = address;
+    public Location(int nameId,int addressId, int imageResourceId){
+        this.nameId = nameId;
+        this.addressId = addressId;
         this.imageResourceId = imageResourceId;
     }
 
-    /*Constructor, initiate variables for Attractions*/
-    public Location(String address,int imageResourceId,String description){
-        this.address = address;
+    /*Constructor, initiate variables for attractions*/
+    public Location(int nameId, int imageResourceId){
+        this.nameId = nameId;
         this.imageResourceId = imageResourceId;
-        this.description = description;
     }
 
-    /*Getters and setters*/
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public int getNameId() {
+        return nameId;
     }
 
-    public String getAddress() {
-        return address;
+    public int getPhoneNumberId() {
+        return phoneNumberId;
     }
 
-    public String getRating() {
-        return rating;
+    public int getAddressId() {
+        return addressId;
     }
 
-    public String getDescription() {
-        return description;
+    public int getDescriptionId() {
+        return descriptionId;
+    }
+
+    public int getRatingId() {
+        return ratingId;
     }
 
     public int getImageResourceId() {
         return imageResourceId;
     }
-
 }
