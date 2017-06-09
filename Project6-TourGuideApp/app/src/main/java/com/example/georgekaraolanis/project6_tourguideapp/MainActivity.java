812edb3098ae761328aa710e87690baa2c800f22
shalantor <georgkaraolanis@gmail.com>
2017-06-09 17:14:23 +0300

    Project6: Show everything on screen through mainactivity class

--- Project6-TourGuideApp/app/src/main/java/com/example/georgekaraolanis/project6_tourguideapp/MainActivity.java
+++ Project6-TourGuideApp/app/src/main/java/com/example/georgekaraolanis/project6_tourguideapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.georgekaraolanis.project6_tourguideapp;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*Get the viewPager*/
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        /*Create an adapter that knows which fragment should be shown on each page*/
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        /*Set the adapter onto the view pager*/
+        viewPager.setAdapter(adapter);
+
+        /*Get the tab layout*/
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        /*Connect TabLayout with view pager*/
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
