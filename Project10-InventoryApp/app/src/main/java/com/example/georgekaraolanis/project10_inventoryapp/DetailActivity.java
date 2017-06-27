93b58c03ec2198633c11b6cbc2f8e94f052d276e
shalantor <georgkaraolanis@gmail.com>
2017-06-27 13:47:56 +0300

    Project10: Show item info except image

--- Project10-InventoryApp/app/src/main/java/com/example/georgekaraolanis/project10_inventoryapp/DetailActivity.java
+++ Project10-InventoryApp/app/src/main/java/com/example/georgekaraolanis/project10_inventoryapp/DetailActivity.java
@@ -1,13 +1,101 @@
 package com.example.georgekaraolanis.project10_inventoryapp;
 
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.LoaderManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity{
+import com.example.georgekaraolanis.project10_inventoryapp.data.InventoryContract;
+import com.example.georgekaraolanis.project10_inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class DetailActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>{
+
+    /*Loader id*/
+    private static final int INVENTORY_LOADER = 1;
+
+    /*Current item*/
+    Uri currentUri;
+
+    /*Views in layout*/
+    ImageView itemImageView;
+    TextView itemNameTextView;
+    TextView itemQuantityTextView;
+    TextView itemPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        /*Get intent data*/
+        Intent intent = getIntent();
+        currentUri = intent.getData();
+
+        getLoaderManager().initLoader(INVENTORY_LOADER, null, this);
+
+        /*references to views*/
+        itemImageView = (ImageView) findViewById(R.id.item_image);
+        itemNameTextView = (TextView) findViewById(R.id.item_name);
+        itemQuantityTextView = (TextView) findViewById(R.id.item_quantity);
+        itemPriceTextView = (TextView) findViewById(R.id.item_price);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Since the editor shows all pet attributes, define a projection that contains
+        // all columns from the pet table
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_ITEM_NAME,
+                InventoryEntry.COLUMN_ITEM_QUANTITY,
+                InventoryEntry.COLUMN_ITEM_PRICE,
+                InventoryEntry.COLUMN_ITEM_IMAGE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                currentUri,         // Query the content URI for the current pet
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+
+        if (cursor.moveToFirst()) {
+            // Find the columns of pet attributes that we're interested in
+            int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
+            int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
+            int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
+            int imageColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_ITEM_IMAGE);
+
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColumnIndex);
+            Integer quantity = cursor.getInt(quantityColumnIndex);
+            Float price = cursor.getFloat(priceColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            itemNameTextView.setText(name);
+            itemQuantityTextView.setText(quantity.toString());
+            itemPriceTextView.setText(price.toString());
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // If the loader is invalidated, clear out all the data from the input fields.
     }
 }
